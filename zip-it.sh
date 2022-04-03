07feda5ac1d7a204d4acebd63839036cb5c68c05
xmb <fxmbsw7@gmail.com>
2022-04-03 18:00:53 -0300

    turn the sh script into a bash script

--- zip-it.sh
+++ zip-it.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env sh
-mkdir -p $INPUT_PKG_DIRECTORIES
+#!/usr/bin/env bash
+[[ $INPUT_PKG_DIRECTORIES ]] && mkdir -p $INPUT_PKG_DIRECTORIES
 sed -i "s/%package%/${INPUT_PACKAGE_NAME}/g" /zip-it;
 set -x
 sed -i "s/%prefix%/${INPUT_PREFIX}/g" /crackle.conf;
@@ -7,7 +7,7 @@ sed -i "s/%package%/${INPUT_PACKAGE_NAME}/g" /crackle.conf;
 sed -i "s/%maintainer%/${INPUT_MAINTAINER}}/g" /crackle.conf;
 mv /zip-it ${INPUT_PACKAGE_NAME}|| exit 1;
 mv $INPUT_BIN bin/
-[ $INPUT_LIB != "" ] && mv $INPUT_LIB lib/
-[ $INPUT_SHARE != "" ] && mv $INPUT_SHARE share
+[[ -e $INPUT_LIB ]] && mv $INPUT_LIB lib/
+[[ -e $INPUT_SHARE ]] && mv $INPUT_SHARE share
 set +x
 zip -r $INPUT_PACKAGE_NAME.zip $INPUT_PKG_DIRECTORIES ${INPUT_PACKAGE_NAME} /crackle.conf
