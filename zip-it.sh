1b28b61e1d9baad98506ba76cfb0247809627079
Rahammetoela Toekiman <fuseteam@duck.com>
2022-04-03 20:55:58 -0300

    take files that go into share/<package_name> into account

--- zip-it.sh
+++ zip-it.sh
@@ -7,5 +7,6 @@ sed -i "s/%package%/${INPUT_PACKAGE_NAME}/g" /zip-it;
 [[ -e $INPUT_BIN ]] && mv $INPUT_BIN bin/
 [[ -e $INPUT_LIB ]] && mv $INPUT_LIB lib/
 [[ -e $INPUT_SHARE ]] && mv $INPUT_SHARE share
+[[ -e $INPUT_DATA ]] && mv $INPUT_DATA share/${INPUT_PACKAGE_NAME}
 mv /zip-it ${INPUT_PACKAGE_NAME}
 zip -r $INPUT_PACKAGE_NAME.zip $INPUT_PKG_DIRECTORIES ${INPUT_PACKAGE_NAME} /crackle.conf
