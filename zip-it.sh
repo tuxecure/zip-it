7b423b39454bb277c6cc9b93038e8386cb5cc2e6
Fuseteam <fusekai@outlook.com>
2022-04-03 16:59:55 -0300

    add an install script in the zip

--- zip-it.sh
+++ zip-it.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 mkdir -p $INPUT_PKG_DIRECTORIES
+sed -i "s/%package%/${INPUT_PACKAGE_NAME}/g" zip-it;
+mv zip-it ${INPUT_PACKAGE_NAME};
 mv $INPUT_BIN bin/
 [ $INPUT_LIB != "" ] && mv $INPUT_LIB lib/
 [ $INPUT_SHARE != "" ] && mv $INPUT_SHARE share
-zip -r $INPUT_PACKAGE_NAME.zip $INPUT_PKG_DIRECTORIES
+zip -r $INPUT_PACKAGE_NAME.zip $INPUT_PKG_DIRECTORIES ${INPUT_PACKAGE_NAME} crackle.conf
