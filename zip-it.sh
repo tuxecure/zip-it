66b9376678b200a2d83cc3005363ffacc8fd5aa5
Rahammetoela Toekiman <fuseteam@duck.com>
2022-04-03 20:41:34 -0300

    create the package script _after_ restructuring

--- zip-it.sh
+++ zip-it.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 [[ $INPUT_PKG_DIRECTORIES ]] && mkdir -p $INPUT_PKG_DIRECTORIES
-sed -i "s/%package%/${INPUT_PACKAGE_NAME}/g" /zip-it;
 sed -i "s|%prefix%|${INPUT_PREFIX}|g" /crackle.conf;
 sed -i "s/%package%/${INPUT_PACKAGE_NAME}/g" /crackle.conf;
 sed -i "s/%maintainer%/${INPUT_MAINTAINER}/g" /crackle.conf;
-mv /zip-it ${INPUT_PACKAGE_NAME}
+sed -i "s/%package%/${INPUT_PACKAGE_NAME}/g" /zip-it;
 [[ -e $INPUT_BIN ]] && mv $INPUT_BIN bin/
 [[ -e $INPUT_LIB ]] && mv $INPUT_LIB lib/
 [[ -e $INPUT_SHARE ]] && mv $INPUT_SHARE share
+mv /zip-it ${INPUT_PACKAGE_NAME}
 zip -r $INPUT_PACKAGE_NAME.zip $INPUT_PKG_DIRECTORIES ${INPUT_PACKAGE_NAME} /crackle.conf
