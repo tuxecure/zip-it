49288a3b41bd0903df6e872bbe5bfecff5d99366
Fuseteam <fusekai@outlook.com>
2022-04-03 18:17:21 -0300

    fix typos

--- zip-it.sh
+++ zip-it.sh
@@ -4,7 +4,7 @@ sed -i "s/%package%/${INPUT_PACKAGE_NAME}/g" /zip-it;
 set -x
 sed -i "s/%prefix%/${INPUT_PREFIX}/g" /crackle.conf;
 sed -i "s/%package%/${INPUT_PACKAGE_NAME}/g" /crackle.conf;
-sed -i "s/%maintainer%/${INPUT_MAINTAINER}}/g" /crackle.conf;
+sed -i "s/%maintainer%/${INPUT_MAINTAINER}/g" /crackle.conf;
 mv /zip-it ${INPUT_PACKAGE_NAME}|| exit 1;
 mv $INPUT_BIN bin/
 [[ -e $INPUT_LIB ]] && mv $INPUT_LIB lib/
