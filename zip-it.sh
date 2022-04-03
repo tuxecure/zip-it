9351c1579f7802d2aceb7037ad413ab218f46af0
Rahammetoela Toekiman <fuseteam@duck.com>
2022-04-03 14:35:09 -0300

    debug error

--- zip-it.sh
+++ zip-it.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
+set -x
 mkdir -p $INPUT_PKG_DIRECTORIES
 mv $INPUT_BIN bin/
 [ -z $INPUT_LIB ] && mv $INPUT_LIB lib/
-[ -z $INPUT_SHARE] && mv $INPUT_SHARE share
+[ -z $INPUT_SHARE ] && mv $INPUT_SHARE share
 zip -r $INPUT_PACKAGE_NAME.zip $INPUT_PKG_DIRECTORIES
