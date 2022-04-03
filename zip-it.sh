845d25403e33bdf773eae9e88191a621032ec975
Fuseteam <fusekai@outlook.com>
2022-04-03 17:24:10 -0300

    kill it with fire

--- zip-it.sh
+++ zip-it.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 mkdir -p $INPUT_PKG_DIRECTORIES
 sed -i "s/%package%/${INPUT_PACKAGE_NAME}/g" zip-it;
-mv zip-it ${INPUT_PACKAGE_NAME};
+mv zip-it ${INPUT_PACKAGE_NAME}|| exit 1;
 mv $INPUT_BIN bin/
 [ $INPUT_LIB != "" ] && mv $INPUT_LIB lib/
 [ $INPUT_SHARE != "" ] && mv $INPUT_SHARE share
