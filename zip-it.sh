7e975dbb9d0b3d09592e4f9abc0a8da381bf6b47
Rahammetoela Toekiman <fuseteam@duck.com>
2022-04-03 14:47:24 -0300

    fix bashism

--- zip-it.sh
+++ zip-it.sh
@@ -3,6 +3,6 @@ set -x
 mkdir -p $INPUT_PKG_DIRECTORIES
 mv $INPUT_BIN bin/
 echo $INPUT_LIB;
-[ -n $INPUT_LIB ] && mv $INPUT_LIB lib/
-[ -n $INPUT_SHARE ] && mv $INPUT_SHARE share
+[ $INPUT_LIB != "" ] && mv $INPUT_LIB lib/
+[ $INPUT_SHARE != "" ] && mv $INPUT_SHARE share
 zip -r $INPUT_PACKAGE_NAME.zip $INPUT_PKG_DIRECTORIES
