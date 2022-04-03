1b46ae63b4dcdab714bda637d3cea368f80e07fd
Fuseteam <fusekai@outlook.com>
2022-04-03 17:27:12 -0300

    attempt to fix #1

--- zip-it.sh
+++ zip-it.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 mkdir -p $INPUT_PKG_DIRECTORIES
-sed -i "s/%package%/${INPUT_PACKAGE_NAME}/g" zip-it;
-mv zip-it ${INPUT_PACKAGE_NAME}|| exit 1;
+sed -i "s/%package%/${INPUT_PACKAGE_NAME}/g" /zip-it;
+mv /zip-it ${INPUT_PACKAGE_NAME}|| exit 1;
 mv $INPUT_BIN bin/
 [ $INPUT_LIB != "" ] && mv $INPUT_LIB lib/
 [ $INPUT_SHARE != "" ] && mv $INPUT_SHARE share
-zip -r $INPUT_PACKAGE_NAME.zip $INPUT_PKG_DIRECTORIES ${INPUT_PACKAGE_NAME} crackle.conf
+zip -r $INPUT_PACKAGE_NAME.zip $INPUT_PKG_DIRECTORIES ${INPUT_PACKAGE_NAME} /crackle.conf
