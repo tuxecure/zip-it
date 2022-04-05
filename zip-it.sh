d41d06951d28173435c21154dac4977957f6fed8
Rahammetoela Toekiman <fuseteam@duck.com>
2022-04-05 01:42:35 -0300

    fix sed take 2

--- zip-it.sh
+++ zip-it.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 [[ $INPUT_PKG_DIRECTORIES ]] && mkdir -p $INPUT_PKG_DIRECTORIES
-sed -i "s|%prefix%|${INPUT_PREFIX}|g" /zip-it;
-sed -i "s|%comprefix%|${INPUT_PREFIX}/share/bash-completion/completions|g" /zip-it;
-sed -i "s/%owner%/${INPUT_OWNER}/g" /zip-it;
-sed -i "s/%package%/${INPUT_PACKAGE_NAME}/g" /zip-it;
-
 [[ -e $INPUT_BIN ]] && mv $INPUT_BIN bin/
 [[ -e $INPUT_LIB ]] && mv $INPUT_LIB lib/
 [[ -e $INPUT_SHARE ]] && mv $INPUT_SHARE share
@@ -17,16 +12,21 @@ rpath=${path/./${INPUT_PREFIX}}
 while read path
 do
 	if [ ${path/#.\/} != "wpm" ]; then
-		sed -i "/rm ${INPUT_PREFIX}/i rmdir $rpath" /zip-it
+		sed -i "/rm %prefix%/i rmdir $rpath" /zip-it
 	fi
 done < <(find . -type f)
 
 while read path
 do
 	if [[ ${path/#.\/} != "." && ${path/#.\/} != "bin" && ${path/#.\/} != "lib" && ${path/#.\/} != "share" ]]; then
-		sed -i "/rm ${INPUT_PREFIX}/i rmdir $rpath" /zip-it
+		sed -i "/rm %prefix%/i rmdir $rpath" /zip-it
 	fi
 done < <(find . -type d)
+
+sed -i "s|%prefix%|${INPUT_PREFIX}|g" /zip-it;
+sed -i "s|%comprefix%|${INPUT_PREFIX}/share/bash-completion/completions|g" /zip-it;
+sed -i "s/%owner%/${INPUT_OWNER}/g" /zip-it;
+sed -i "s/%package%/${INPUT_PACKAGE_NAME}/g" /zip-it;
 
 mv /zip-it ${INPUT_PACKAGE_NAME}
 zip -r $INPUT_PACKAGE_NAME.zip $INPUT_PKG_DIRECTORIES ${INPUT_PACKAGE_NAME}
