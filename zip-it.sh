9241ffe38454118394359bd3e4e69d8b6c956f74
Rahammetoela Toekiman <fuseteam@duck.com>
2022-04-17 13:52:49 -0300

    improve config

--- zip-it.sh
+++ zip-it.sh
@@ -4,9 +4,8 @@ fi
 [[ $INPUT_PKG_DIRECTORIES ]] && mkdir -p $INPUT_PKG_DIRECTORIES
 [[ -e $INPUT_BIN ]] && mv $INPUT_BIN bin/
 if [[ -e $INPUT_CONFIG ]]; then
-	mv $INPUT_CONFIG ${INPUT_PACKAGE_NAME}.conf
-	mkdir config
-	mv ${INPUT_PACKAGE_NAME}.conf config/${INPUT_PACKAGE_NAME}
+	mkdir -p config/${INPUT_PACKAGE_NAME}
+	mv ${INPUT_CONFIG} config/${INPUT_PACKAGE_NAME}
 fi
 [[ -e $INPUT_LIB ]] && mv $INPUT_LIB lib/
 [[ -e $INPUT_SHARE ]] && mv $INPUT_SHARE share
