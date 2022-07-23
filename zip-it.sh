6c9456a13e1dff5dd62af8c95fe6a155c459060a
mazunki <rolferen@gmail.com>
2022-07-23 07:36:27 +0200

    fixed mandatory vars

--- zip-it.sh
+++ zip-it.sh
@@ -3,13 +3,24 @@ set -e
 PREFIX="${PREFIX:-/}"
 TARGET_PATH="./"
 
-mkdir -p "${INPUT_PKG_DIRECTORIES?:"This script currently doesn't work outside of GitHub"}"
+# thanks to github magic, we declare variables in the actions.yml file
+# which then turn into ${INPUT_thename} in the environment here
+
+mkdir -p "${INPUT_PKG_DIRECTORIES?:"The directories to be zipped was not set"}"
+
+# checking for mandatory flags (can technically be empty, but not null)
+"${INPUT_OWNER?"There was no owner specified"}"  # authorship
+"${INPUT_REPO_NAME?"There was no repository name specified"}"  # used for tracking the source of the program
+"${INPUT_PKGREL?"There was no package release specified"}"  # the version number
+"${INPUT_PKG_DIRECTORIES?"There was no package directories specified"}"  # an array of files to be included in the zip
+"${INPUT_PACKAGE_NAME?"There was no package name specified"}"  # used for the zipname and stuff
+
 
 # moving all the input directories into the prefixed locations
-[ -e "${INPUT_BIN:?}" ] && mv -t "${PREFIX}/bin/" "${INPUT_BIN}"
-[ -e "${INPUT_LIB:?}" ] && mv -t "${PREFIX}/lib/" "${INPUT_LIB}"
-[ -e "${INPUT_SHARE:?}" ] && mv -t "${PREFIX}/share/" "${INPUT_SHARE}"
-if [ -e "${INPUT_CONFIG:?}" ]; then
+[ -e "${INPUT_BIN}" ] && mv -t "${PREFIX}/bin/" "${INPUT_BIN}"
+[ -e "${INPUT_LIB}" ] && mv -t "${PREFIX}/lib/" "${INPUT_LIB}"
+[ -e "${INPUT_SHARE}" ] && mv -t "${PREFIX}/share/" "${INPUT_SHARE}"
+if [ -e "${INPUT_CONFIG}" ]; then
 	mkdir -p "${PREFIX}/config/${INPUT_PACKAGE_NAME}"
 	mv -t "${PREFIX}/config/${INPUT_PACKAGE_NAME}" "${INPUT_CONFIG}"
 fi
@@ -17,7 +28,7 @@ fi
 # uh
 if [ -e "${INPUT_DATA_DIRECTORY}" ]; then
 	mkdir -p "${PREFIX}/share/${INPUT_PACKAGE_NAME}"
-	mv "share/${INPUT_PACKAGE_NAME}" "${INPUT_DATA_DIRECTORY}"
+	mv -t "share/${INPUT_PACKAGE_NAME}" "${INPUT_DATA_DIRECTORY}"
 fi
 
 # let's make a place we can pollute, and work there instead
